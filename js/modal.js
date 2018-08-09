d6cc84b73278a7a1517deff60906e1cec548e626
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-08-09 23:24:09 +0300

    Добавляет Java Script для оживления модальных окон

--- js/modal.js
+++ js/modal.js
@@ -3,16 +3,17 @@
 (function() {
   var escCode = 27;
 
+  var link = document.querySelector('.nav-user__link');
+  var authorizationPopup = document.querySelector('.authorization');
+  var login = document.querySelector('[name=user-login]');
+  var modalClose = document.querySelector('.modal__close');
+
   var escPressHandler = function (evt) {
     if (evt.keyCode === escCode) {
       authorizationPopup.classList.remove('show');
+      authorizationPopup.classList.remove('error');
     }
   };
-
-  var link = document.querySelector('.nav-user__link');
-  var authorizationPopup = document.querySelector('.authorization');
-  var modalClose = document.querySelector('.modal__close');
-  var login = document.querySelector('[name=user-login]')
 
   link.addEventListener('click', function (evt) {
     evt.preventDefault();
@@ -25,5 +26,6 @@
   modalClose.addEventListener('click', function (evt) {
     evt.preventDefault();
     authorizationPopup.classList.remove('show');
+    authorizationPopup.classList.remove('error');
   });
 })();
