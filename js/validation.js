07bb153d1bd467bedbf7410a6ff0a7ab4d97ac8e
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-08-09 23:24:54 +0300

    Добавляет Java Script для валидации формы авторизации

--- js/validation.js
+++ js/validation.js
@@ -1,5 +1,17 @@
 'use strict';
 
 (function () {
+  var authorizationPopup = document.querySelector('.authorization');
+  var form = document.querySelector('.authorization__form');
+  var userLogin = form.querySelector('[name=user-login]');
+  var userPassword = form.querySelector('[name=user-password]');
 
+  form.addEventListener('submit', function (evt) {
+    if (!userLogin.value || !userPassword.value) {
+      evt.preventDefault();
+      authorizationPopup.classList.remove('error');
+      void authorizationPopup.offsetWidth;
+      authorizationPopup.classList.add('error');
+    }
+  });
 })();
